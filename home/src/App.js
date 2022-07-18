d7a155d458b1b44696f820c3ecbbd18489624dfa
codenamenam <bluetire27@gmail.com>
2022-07-18 17:26:41 +0900

    Working on handling card-click event, implementing horizontal scroll to show dog/cat breeds

--- home/src/App.js
+++ home/src/App.js
@@ -1,14 +1,15 @@
+/* eslint-disable */
+
 import "./App.css";
-import Tail from "./component/Tail";
 import Title from "./component/Title";
 import Body from "./component/Body";
+import Breed from "./component/Breed";
 
 function App() {
   return (
     <div>
       <Title />
       <Body />
-      <Tail />
     </div>
   );
 }
--- home/src/component/Body.js
+++ home/src/component/Body.js
@@ -1,11 +1,18 @@
 /* eslint-disable */
 import "../App.css";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import React from "react";
+import Breed from "./Breed";
+import ReactDOM from "react-dom";
 
-const selectCard = (name) => {
-  console.log("Hello. " + name);
-};
+function selectCard(type) {
+  if (type == 1) {
+    console.log("Cat 선택");
+    ReactDOM.render(<Breed type="1" />), Document.getElementById("message");
+  } else {
+    console.log("Dog 선택");
+    ReactDOM.render(<Breed type="2" />), Document.getElementById("message");
+  }
+}
 
 const Body = () => {
   return (
@@ -16,7 +23,7 @@ const Body = () => {
       <div class="col d-flex justify-content-center">
         <div class="row">
           <div class="col-6">
-            <div class="card" onClick={() => selectCard("cat")} click>
+            <div class="card" onClick={() => selectCard(1)} click>
               <img
                 class="card-img-top"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg"
@@ -30,7 +37,7 @@ const Body = () => {
           </div>
 
           <div class="col-6">
-            <div class="card" onClick={() => selectCard("dog")}>
+            <div class="card" onClick={() => selectCard(2)}>
               <img
                 src="https://cdn.crowdpic.net/detail-thumb/thumb_d_382A8A747FFDF073E20C13398D110DE7.jpg"
                 alt=""
